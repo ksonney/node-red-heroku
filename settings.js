164dfaa6ee03dd64dee6ed966d020c10a0d9dc86
Kevin Sonney <kevin@sonney.com>
2017-10-17 16:30:02 -0400

    remove static content and just go right in

--- settings.js
+++ settings.js
@@ -34,9 +34,9 @@ var settings = module.exports = {
     autoInstallModules: true,
 
     // Move the admin UI
-    httpAdminRoot: '/red',
+    // httpAdminRoot: '/red',
 
-    httpNodeRoot: '/red-nodes',
+    // httpNodeRoot: '/red-nodes',
 
     // You can protect the user interface with a userid and password by using the following property
     // the password must be an md5 hash  eg.. 5f4dcc3b5aa765d61d8327deb882cf99 ('password')
